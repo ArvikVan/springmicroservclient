416264040177213ec6a4e70e9b9fb851f931d3d4
ArvikVan <arvikvan@gmail.com>
2022-03-30 21:25:50 +0300

    initail

---
+++ src/main/java/arvik/springmicroservclient/SpringmicroservclientApplication.java
@@ -0,0 +1,13 @@
+package arvik.springmicroservclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmicroservclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmicroservclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arvik/springmicroservclient/SpringmicroservclientApplicationTests.java
@@ -0,0 +1,13 @@
+package arvik.springmicroservclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmicroservclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
